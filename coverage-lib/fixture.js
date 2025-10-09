a33b290ebf757f6605f7b96658a150ec464298d8
rattus-aristarchus <lankin.m.a@gmail.com>
2025-10-09 10:34:51 +0300

    removed chaff

--- coverage-lib/fixture.js
+++
@@ -1,5 +0,0 @@
-// coverage-lib/fixture.js - Простой импорт для UI Test Coverage
-
-export { test, expect } from './testCoverageFixture.js';
-export { TestElementTracker } from './testElementTracker.js'; 
-export { GlobalCoverageTracker } from './globalCoverageTracker.js';
